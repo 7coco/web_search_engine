f836b09438dc131f9b1787a9e7cc9bcd41aad56a
7coco <coco.greenery@gmail.com>
2017-03-27 12:02:23 +0900

    Format

--- FileSearch1.java
+++ FileSearch1.java
@@ -6,10 +6,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class FileSearch1
-{
+public class FileSearch1 {
 	public static void main(String[] args) {
-		if(args.length != 2){
+		if (args.length != 2) {
 			System.out.println("検索したい文字列と検索対象テキストファイル名を空白区切で入力してください.");
 			System.out.println("例）hoge test.txt");
 			System.exit(1);
@@ -38,7 +37,7 @@ public class FileSearch1
 					result += "\n";
 				 	result += lineNumber + "行目の";
 					int charNumber = 0;
-					while(str.indexOf(query, charNumber) != -1){
+					while (str.indexOf(query, charNumber) != -1) {
 						charNumber = str.indexOf(query, charNumber) + 1;
 						result += charNumber + "文字目 ";
 					}
@@ -47,5 +46,5 @@ public class FileSearch1
 			}
 			return result;
 		}
-  	}
+	}
 }
