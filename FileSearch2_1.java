a9f02903be88e810e6f6c0c1770b9f13526e92eb
7coco <coco.greenery@gmail.com>
2017-03-28 13:50:41 +0900

    Implement FileSearcher class and move to it search method and createoutputStr method

--- FileSearch2_1.java
+++ FileSearch2_1.java
@@ -1,15 +1,7 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-
-public class Hoge() {
-	
-}
 
 public class FileSearch2_1 {
 	public static void main(String[] args) {
@@ -21,13 +13,14 @@ public class FileSearch2_1 {
 		String[] fileNames = Arrays.copyOfRange(args, 1, args.length);
 		for (String fileName : fileNames) {
 			try {
-				Map<Integer, List<Integer>> result = search(args[0], fileName);
+				FileSearcher fs = new FileSearcher(fileName);
+				Map<Integer, List<Integer>> result = fs.search(args[0]);
 				System.out.println("ファイル名：" + fileName);
 				if (result.isEmpty()) {
 					System.out.println("該当する行はありませんでした。");
 				} else {
 					System.out.println("入力された文字列が現れた行番号は以下の通りです。");
-					String answer = createOutputStr(result);
+					String answer = fs.createOutputStr(result);
 					System.out.print(answer);
 				}
 			} catch (IOException e) {
@@ -37,57 +30,4 @@ public class FileSearch2_1 {
 		}
 	}
 
-	/**
-	 * 指定されたファイルから指定された文字列の位置を検索します。
-	 * 
-	 * @param query
-	 *            検索文字列
-	 * @param fileName
-	 *            検索対象のファイル名
-	 * @return 検索結果。キー：行数、値：文字の位置リスト
-	 * @throws IOException
-	 */
-	private static Map<Integer, List<Integer>> search(String query, String fileName) throws IOException {
-		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-			Map<Integer, List<Integer>> result = new LinkedHashMap<Integer, List<Integer>>();
-			String str;
-			int lineNumber = 1;
-			while ((str = br.readLine()) != null) {
-				if (str.indexOf(query) == -1) {
-					lineNumber++;
-					continue;
-				}
-				List<Integer> charNumbers = new ArrayList<Integer>();
-				int charNumber = 0;
-				while ((charNumber = str.indexOf(query, charNumber)) != -1) {
-					charNumber++;
-					charNumbers.add(charNumber);
-				}
-				result.put(lineNumber, charNumbers);
-				lineNumber++;
-			}
-			return result;
-		}
-	}
-
-	/**
-	 * 引数のマップから最終的に出力する文字列を作ってそれを返します。
-	 * 
-	 * @param resultMap
-	 *            検索結果のマップ
-	 * @return 出力するべき文字列
-	 */
-	private static String createOutputStr(Map<Integer, List<Integer>> resultMap) {
-		StringBuilder answer = new StringBuilder();
-		for (Map.Entry<Integer, List<Integer>> entry : resultMap.entrySet()) {
-			int line = entry.getKey();
-			List<Integer> chars = entry.getValue();
-			answer.append(line).append("行目 ");
-			for (Integer c : chars) {
-				answer.append(c).append("文字目 ");
-			}
-			answer.append("\n");
-		}
-		return answer.toString();
-	}
 }
