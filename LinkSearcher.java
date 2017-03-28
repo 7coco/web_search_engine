21e45a5801e32a5465e56d7534551461f4db6dc0
7coco <coco.greenery@gmail.com>
2017-03-28 18:26:27 +0900

    Add LInkSearcher.java

---
+++ LinkSearcher.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.regex.Pattern;
+
+public class LinkSearcher {
+	private URL url;
+
+	public LinkSearcher(String url) throws MalformedURLException {
+		this.url = new URL(url);
+	}
+
+	public String search(URL url, Pattern regexp) throws IOException {
+		InputStream is = url.openStream();
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+			String str;
+			while ((str = br.readLine()) != null) {
+				// ここでsearch を行う。
+			}
+		}
+
+	}
+}
