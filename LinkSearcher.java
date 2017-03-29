8fe6d549b78492ac0ae6af43a9ba97bee24cc639
7coco <coco.greenery@gmail.com>
2017-03-29 11:13:19 +0900

    Add import

--- LinkSearcher.java
+++ LinkSearcher.java
@@ -4,6 +4,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LinkSearcher {
