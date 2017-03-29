e0f437b8055f6d80bf87b5594149d1ebdc11cade
7coco <coco.greenery@gmail.com>
2017-03-29 11:37:48 +0900

    Call LinkSearcher and its methods

--- WebCrawler1.java
+++ WebCrawler1.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.List;
 
 public class WebCrawler1 {
 	public static void main(String[] args) {
@@ -7,6 +8,9 @@ public class WebCrawler1 {
 			System.exit(1);
 		}
 		try {
+			LinkSearcher ls = new LinkSearcher(args[0]);
+			List<String> links = ls.search();
+			System.out.print(ls.createOutputStr(links));
 		} catch (IOException e) {
 			System.out.println("入出力エラーです。");
 			e.printStackTrace();
