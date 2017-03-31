9308c2df0ab247cd055e1ace9c8fb4f3753bb3c7
7coco <coco.greenery@gmail.com>
2017-03-31 10:40:49 +0900

    Add import

--- LinkSearcher.java
+++ LinkSearcher.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
