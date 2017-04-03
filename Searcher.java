d03d912894f8b8abb0b0c095414fe3dfab533177
7coco <coco.greenery@gmail.com>
2017-04-03 15:36:59 +0900

    Add Searcher.java by copying LinkSearcher.java

---
+++ Searcher.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Searcher {
+	private URL url;
+	static private final Pattern LINK_REGEXP = Pattern
+			.compile("<a href=\"([^\"]*html?)\"|<frame[^<]src=\"([^\"]*\\.html?)");
+	static private final Pattern TARGET_URL_REGEXP = Pattern.compile("https?");
+	private Set<URL> referenceadUrls;
+
+	public Searcher(String url) throws MalformedURLException {
+		this.url = new URL(url);
+		this.referenceadUrls = new HashSet<URL>();
+	}
+
+	public Set<URL> search(URL url) throws IOException {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
+			Set<URL> searchedUrls = new HashSet<URL>();
+			String str;
+			while ((str = br.readLine()) != null) {
+				Matcher m = LINK_REGEXP.matcher(str);
+				if (m.find()) {
+					String link = (m.group(1) != null) ? m.group(1) : m.group(2);
+					try {
+						URL searchedUrl = this.url.toURI().resolve(link).toURL();
+						if (searchedUrl.toString().startsWith(this.url.toString())
+								&& TARGET_URL_REGEXP.matcher(this.url.toString()).find()) {
+							searchedUrls.add(searchedUrl);
+						}
+					} catch (URISyntaxException e) {
+						continue;
+					}
+				}
+			}
+			searchedUrls = deleteDuplicates(searchedUrls);
+			referenceadUrls.addAll(searchedUrls);
+			searchedUrls = forSearch(searchedUrls);
+			return searchedUrls;
+		}
+	}
+
+	private Set<URL> deleteDuplicates(Set<URL> urls) {
+		urls.removeAll(referenceadUrls);
+		return urls;
+	}
+
+	private Set<URL> forSearch(Set<URL> urls) {
+		Set<URL> resultUrls = new HashSet<>();
+		for (URL url : urls) {
+			System.out.println(url);
+			try {
+				resultUrls.addAll(search(url));
+			} catch (IOException e) {
+				continue;
+			}
+		}
+		return resultUrls;
+	}
+
+	public String createOutputStr(Set<URL> searchedUrls) {
+		StringBuilder outputStr = new StringBuilder();
+		for (URL url : searchedUrls) {
+			outputStr.append(url).append("\n");
+		}
+		return outputStr.toString();
+	}
+}
